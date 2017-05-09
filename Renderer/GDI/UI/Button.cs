9c43d0064b78c144843646bf34e2c0099c364ef8
hrost <hrost@ROE-147>
2017-05-09 16:02:02 +0200

    Button als mögliches Control hinzugefügt.

---
+++ Renderer/GDI/UI/Button.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using Mathematics.Vector;
+
+namespace Renderer.GDI.UI
+{
+
+    /// <summary>
+    /// Repräsentation eines Button innerhalb eines GDI User-Interfaces.
+    /// 
+    /// Autor: Hendrik Rost
+    /// </summary>
+    public class Button : Control
+    {
+        public Button(Vector2 Size, Vector2 Location)
+            : base(Size, Location, Color.Black)
+        {
+
+
+        }
+    }
+}
